d245ede4f056b3104d924016a5a7d83e133b7f07
MariamO6274 <ostatishvili@gmail.com>
2023-05-21 12:56:39 -0600

    fixing images
    took out copy URL

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 // import TodoList from "./TodoList";
 import Card from "./Card";
 import "./Style.css";
-// import "./App.css";
-// import siri_logo from "./images/siri_logo.png";
+import siri_logo from "./images/siri_logo.png";
 import alexa_logo from "./images/alexa_logo.png";
-// import cortana_logo from "./images/cortana_logo.png";
+import cortana_logo from "./images/cortana_logo.png";
 
 function App() {
   return (
@@ -14,18 +13,19 @@ function App() {
         title="Alexa"
         user="@alexa99"
         description="I'll help you buy stuff off Amazon"
-        imageUrl={alexa_logo}/>
+        imageUrl={alexa_logo}
+      />
       <Card
         title="Cortana"
         user="@cortana12"
         description="Personal assistant by Microsoft"
-        imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Microsoft_Cortana_transparent.svg/1200px-Microsoft_Cortana_transparent.svg.png"
+        imageUrl={cortana_logo}
       />
       <Card
         title="Siri"
         user="@siri44"
         description="I don't get a lot of updates anymore"
-        imageUrl="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/homepod/ios14-siri-icon.png"
+        imageUrl={siri_logo}
       />
     </div>
   );
