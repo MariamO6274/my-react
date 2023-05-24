8fe40a7d7db5ff8e8ea70503cc5f94755c382708
MariamO6274 <ostatishvili@gmail.com>
2023-05-23 18:36:31 -0600

    fixing app2, adding img

--- src/App2.js
+++ src/App2.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import RandomGenerator from './RandomGenerator';
+import React from "react";
+import RandomGenerator from "./RandomGenerator";
+import { useState } from "react";
+import "./App.css";
 
+const imageArr = ["tester1", "tester2", "tester3", "tester4"];
+
+//get a random index
+var randomPhotoIndex = Math.floor(Math.random() * imageArr.length);
+
+//get an image at the randomPhotoIndex
+var selectedPhoto = imageArr[randomPhotoIndex];
+
+// var randomPhotoIndex = Math.floor();
+// var myRange = randomPhotoIndex * imageArr.length;
+// var myIndex = Math.floor(myRange)
 
 function App2() {
+  var counter = 0;
+
+  const [imageCount, setImageCount] = useState(0);
+
+  var imageList = [];
+
+  for (let i = 0; i < imageCount; i++) {
+    imageList.push(<RandomGenerator type={imageArr[selectedPhoto]} />);
+  }
+
+  function getMoreImages() {
+    counter += 1;
+    setImageCount(imageCount + 1);
+  }
+
   return (
     <>
+      <button className="randomButton" onClick={getMoreImages}>
+        Random Image
+      </button>
+
       <RandomGenerator />
-      <button onClick>Random Image</button>
+      <br></br>
+      {imageList}
     </>
   );
 }
 
-export default App2
+export default App2;
--- src/RandomGenerator.js
+++ src/RandomGenerator.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import tester1 from "./images/tester1.jpg";
+import tester2 from "./images/tester2.jpg";
+import tester3 from "./images/tester3.jpg";
+import tester4 from "./images/tester4.jpg"
 
-function RandomGenerator() {
+const testerImages = {
+  "tester1": tester1,
+  "tester2": tester2,
+  "tester3": tester3,
+  "tester4": tester4
+};
+
+const RandomGenerator = ({ type }) => {
   return (
-    <>RandomGenerator</>
-  )
-}
+    <div className="mainContainer">
+      <img src={testerImages[type]}></img>
+    </div>
+  );
+};
 
-export default RandomGenerator
+
+
+export default RandomGenerator;
