e16c9492cd9f3ddbc9e35b342e9750d67fc7e5dc
MariamO6274 <ostatishvili@gmail.com>
2023-06-17 18:46:17 -0600

    ClassBasedComponent

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
+
+// 1. Import React and ReactDOM libraries
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 // import App2 from "./App2";
 // import { App3 } from "./App3";
-// import  BookApp from "./BookApp";
-import AppReducer from "./useReducerApp/AppReducer"
+// // import  BookApp from "./BookApp";
+// import AppReducer from "./useReducerApp/AppReducer"
+import ClassBasedComponent from "./ClassBasedComponent";
 
 
+
+// 2. Get control of your root div element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<App />);
-
 // root.render(<App2/>);
-
 // root.render(<App3 />);
-
 // root.render(<BookApp />);
-root.render(<AppReducer />);
+// root.render(<AppReducer />);
+
+root.render(<ClassBasedComponent name="Tom" lastName="Hank" />);
