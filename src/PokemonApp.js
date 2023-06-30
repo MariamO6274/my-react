1f8d8a097edc53f3f6dbb028c3fff95608591883
MariamO6274 <ostatishvili@gmail.com>
2023-06-29 18:30:29 -0600

    pokemon useEffect

--- src/PokemonApp.js
+++ src/PokemonApp.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import PokemonInfo from './PokemonInfo'
-import axios from 'axios';
+import React, { useEffect } from "react";
+import PokemonInfo from "./PokemonInfo";
+import axios from "axios";
+import { useState } from "react";
 
-function  PokemonApp () {
+function PokemonApp() {
+  const [counter, setCounter] = useState(0);
 
+  const [data, setData] = useState()
+
+  useEffect(() => {
     //axios returns a promise, therefor we need await (+ async) or .then
-  axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((result) => {
-    console.log(result);
-  });
+    axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((result) => {
+      console.log(result.data); // store to state varable
+      setData(result.data)
+    });
+  }, [counter]);
+
   return (
-    <div>PokemonApp
-        <PokemonInfo/>
+    <div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Change it
+      </button>
+      <br />
+      <br />
+      {counter}
+      <br />
+      <br />
+      <h4>Pocamon name is </h4>
+      {data.name}
+      <PokemonInfo />
     </div>
-  )
+  );
 }
 
-export default PokemonApp
+export default PokemonApp;
 
 // if we don't want to use async/await we can youc promises
-// evrytime we use axios or fetch api we should never use without protection 
-// we have to incapsulated in useEffect 
+// evrytime we use axios or fetch api we should never use without protection
+// we have to incapsulated in useEffect
